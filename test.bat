95497fc9cefe749fab43fb4e02f86ea35ba5aff2
CORP\fayuli <fayuan.li@capgemini.com>
2021-02-24 20:13:10 +0800

    exeTestBAT

---
+++ test.bat
@@ -0,0 +1,4 @@
+echo "teststart"
+::newman run C:\Users\fayuli\Desktop\postmanResult\dudu.postman_collection.json -d dudu.postman_collection.json -r html --reporter-html-export C:\Users\fayuli\Desktop\postmanResult\result.html --bail newman
+echo 112234
+newman run C:\Users\fayuli\Desktop\postmanResult\dudu.postman_collection.json -r html
